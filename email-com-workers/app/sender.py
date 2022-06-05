6e895da8f0c6b02e550de1e9b077411180b00e69
Vitor Lucas Crispim <vitor.lucas@nscreen.com.br>
2022-06-05 17:31:14 -0300

    Criando o worker que vai realizar o envio de mails

--- email-com-workers/app/sender.py
+++ email-com-workers/app/sender.py
@@ -1,29 +1,41 @@
 import psycopg2
-from bottle import route, run, request
-
-DSN = 'dbname=email_sender user=postgres password=1 host=db'
-SQL = 'INSERT INTO emails (assunto,mensagem) VALUES (%s,%s)'
-
-def register_mensage(assunto,mensagem):
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL,(assunto,mensagem))
-    conn.commit()
-    cur.close()
-    conn.close()
-    print('Mensagem Salva no Banco! ')
+from bottle import Bottle, request
+import redis
+import json  
 
 
 
 
-@route('/', method = 'POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
-    register_mensage(assunto,mensagem)
-    return 'Mensagem enfileirada! Assunto : {} Mensagem: {} '.format(assunto,mensagem)
+
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/',method='POST', callback=self.send)
+        self.fila = redis.StrictRedis(host='queue',port = 6379,db=0)
+        DSN = 'dbname=email_sender user=postgres password=1 host=db'
+        self.conn=psycopg2.connect(DSN)
+
+
+    def register_mensage(self,assunto,mensagem):
+        SQL = 'INSERT INTO emails (assunto,mensagem) VALUES (%s,%s)'    
+        cur = self.conn.cursor()
+        cur.execute(SQL,(assunto,mensagem))
+        self.conn.commit()
+        cur.close()
+        mensagem = {'assunto':assunto,'mensagem':mensagem}
+        self.fila.rpush('sender',json.dumps(mensagem))
+        print('Mensagem Salva no Banco! ')
+
+
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+        self.register_mensage(assunto,mensagem)
+        return 'Mensagem enfileirada! Assunto : {} Mensagem: {} '.format(assunto,mensagem)
 
 
 
 if __name__ == '__main__':
-    run(host='0.0.0.0',port = 8080, debug = True)
+    sender = Sender()
+    sender.run(host='0.0.0.0',port = 8080, debug = True)
